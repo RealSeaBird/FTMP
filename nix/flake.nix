709c52480282e147d3d4ecb4e958d6f66ad8dbd8
RSB <arcbash@proton.me>
2025-12-25 01:20:09 +0200

    Fixed nix flake and Cmake made basic structure for TUI

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,28 +3,43 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   
-  outputs = { self, nixpkgs }:
-  let
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-
-  in
-  {
-    devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
           gcc
-          ftxui
           cmake
-
-
+          pkg-config
+          
+          # FTXUI - TUI library
+          ftxui
+          
+          # SDL2 and related libraries for audio
+          SDL2
+          SDL2_sound
+          SDL2_mixer
+          SDL2_mixer.dev
+          
+          # Additional build tools
+          ninja
+          gdb
         ];
-
-
+        
+        # Ensure pkg-config can find SDL2 libraries
+        shellHook = ''
+          export PKG_CONFIG_PATH="${pkgs.SDL2}/lib/pkgconfig:${pkgs.SDL2_mixer}/lib/pkgconfig:$PKG_CONFIG_PATH"
+          export LD_LIBRARY_PATH="${pkgs.SDL2}/lib:${pkgs.SDL2_mixer}/lib:$LD_LIBRARY_PATH"
+          echo "FTMP development environment loaded"
+        '';
       };
-
-
-    };
+    }
+  );
 }
--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 #include <ctime>
 #include <stdbool.h>
 #include <algorithm>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
 using namespace std;
 using namespace ftxui;
@@ -21,11 +23,83 @@ auto screen = ScreenInteractive::Fullscreen();
 
 
 
+
+
+
+vector<string> playlists = {
+  "stuff",
+    "more stuff",
+    "this is just a test"
+
+
+
+};
+
+vector<string> songs = {
+  "Magical cure loveshot",
+  "kyu-kuratin",
+  "TRAFFIK!"
+
+
+};
+
+
+
+int playlist_selcted = 0;
+int song_selected = 0;
+int volume = 60;
+string example_song = "Magical cure loveshot";
+
+
+
+
 int main(){
 
-  auto renderer = Renderer([&]{
+  auto playlists_menu = Radiobox(&playlists, &playlist_selcted);
+  auto songs_menu = Radiobox(&songs, &song_selected);
+  auto play_button = Button("▶ Play", [&]{ /* call your play function */ });
+  auto pause_button = Button("⏸ Pause", [&]{ /* call your pause function */ });
+  auto next_button = Button("⏭ Next", [&]{ /* call your next function */ });
+  auto prev_button = Button("⏮ Previous", [&]{ /* call your previous function */ });
+  auto volume_slider = Slider("Volume:", &volume, 0, 100, 1);
+
+  auto container = Container::Vertical({
+    Container::Horizontal(Components{playlists_menu}),
+    Container::Horizontal(Components{play_button, pause_button}),
+    Container::Horizontal(Components{next_button, prev_button}),
+    Container::Horizontal(Components{volume_slider})
+  });
+
+
+
+  if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+    std::cerr << "Failed to initialize SDL: " << SDL_GetError() << "\n";
+    return 1;
+  }
+
+  if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+    std::cerr << "Failed to initialize SDL_mixer: " << Mix_GetError() << "\n";
+    return 1;
+  }
+
+
+
+
+
+
+  auto renderer = Renderer(container,[&]{
+    auto menu_playlist = playlists_menu-> Render();
+    auto menu_song = songs_menu-> Render();
+    auto button_play = play_button-> Render();
+    auto button_pause = pause_button-> Render();
+    auto button_next = next_button-> Render();
+    auto button_prev = prev_button-> Render();
+    auto slider_volume = volume_slider-> Render();
+
     auto test = vbox({
-          text("FTMP") | bold
+          text("Currently playing: " + example_song) | bold | border
+
+
 
 
 
@@ -39,7 +113,25 @@ int main(){
 
 
 
-    return test;
+    return vbox({
+      test,
+      hbox ({
+        menu_playlist | border | flex,
+        menu_song | border | flex
+      }),
+      hbox({
+        button_prev,
+        button_play,
+        button_pause,
+        button_next,
+        slider_volume | border
+
+
+      }) | bold | border
+
+
+
+    });
 
   });
 
